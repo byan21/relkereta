7242a5e7f8e7dbb673212bb553525866650ba482
byan21 <vabyan702@gmail.com>
2017-11-26 04:36:27 -0700

    versi 1.0.1@

---
+++ kereta_1.py
@@ -0,0 +1,179 @@
+import os
+from gps import *
+from time import *
+import time
+import threading
+import smbus
+from time import sleep
+
+# select the correct i2c bus for this revision of Raspberry Pi
+revision = ([l[12:-1] for l in open('/proc/cpuinfo','r').readlines() if l[:8]=="Revision"]+['0000'])[0]
+bus = smbus.SMBus(1 if int(revision, 16) >= 4 else 0)
+
+# ADXL345 constants
+EARTH_GRAVITY_MS2   = 9.80665
+SCALE_MULTIPLIER    = 0.004
+
+DATA_FORMAT         = 0x31
+BW_RATE             = 0x2C
+POWER_CTL           = 0x2D
+
+BW_RATE_1600HZ      = 0x0F
+BW_RATE_800HZ       = 0x0E
+BW_RATE_400HZ       = 0x0D
+BW_RATE_200HZ       = 0x0C
+BW_RATE_100HZ       = 0x0B
+BW_RATE_50HZ        = 0x0A
+BW_RATE_25HZ        = 0x09
+
+RANGE_2G            = 0x00
+RANGE_4G            = 0x01
+RANGE_8G            = 0x02
+RANGE_16G           = 0x03
+
+MEASURE             = 0x08
+AXES_DATA           = 0x32
+
+class ADXL345:
+
+    address = None
+
+    def __init__(self, address = 0x53):        
+        self.address = address
+        self.setBandwidthRate(BW_RATE_100HZ)
+        self.setRange(RANGE_2G)
+        self.enableMeasurement()
+
+    def enableMeasurement(self):
+        bus.write_byte_data(self.address, POWER_CTL, MEASURE)
+
+    def setBandwidthRate(self, rate_flag):
+        bus.write_byte_data(self.address, BW_RATE, rate_flag)
+
+    # set the measurement range for 10-bit readings
+    def setRange(self, range_flag):
+        value = bus.read_byte_data(self.address, DATA_FORMAT)
+
+        value &= ~0x0F;
+        value |= range_flag;  
+        value |= 0x08;
+
+        bus.write_byte_data(self.address, DATA_FORMAT, value)
+    
+    # returns the current reading from the sensor for each axis
+    #
+    # parameter gforce:
+    #    False (default): result is returned in m/s^2
+    #    True           : result is returned in gs
+    def getAxes(self, gforce = False):
+        bytes = bus.read_i2c_block_data(self.address, AXES_DATA, 6)
+        
+        x = bytes[0] | (bytes[1] << 8)
+        if(x & (1 << 16 - 1)):
+            x = x - (1<<16)
+
+        y = bytes[2] | (bytes[3] << 8)
+        if(y & (1 << 16 - 1)):
+            y = y - (1<<16)
+
+        z = bytes[4] | (bytes[5] << 8)
+        if(z & (1 << 16 - 1)):
+            z = z - (1<<16)
+
+        x = x * SCALE_MULTIPLIER 
+        y = y * SCALE_MULTIPLIER
+        z = z * SCALE_MULTIPLIER
+
+        if gforce == False:
+            x = x * EARTH_GRAVITY_MS2
+            y = y * EARTH_GRAVITY_MS2
+            z = z * EARTH_GRAVITY_MS2
+
+        x = round(x, 4)
+        y = round(y, 4)
+        z = round(z, 4)
+
+        return {"x": x, "y": y, "z": z}
+ 
+gpsd = None #seting the global variable
+ 
+os.system('clear') #clear the terminal (optional)
+ 
+class GpsPoller(threading.Thread):
+  def __init__(self):
+    threading.Thread.__init__(self)
+    global gpsd #bring it in scope
+    gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
+    self.current_value = None
+    self.running = True #setting the thread running to true
+ 
+  def run(self):
+    global gpsd
+    while gpsp.running:
+      gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
+
+def index(kecepatan, aX):
+    if (kecepatan >= 80):
+        nh=15//aX
+    elif (kecepatan >= 40 and 80 > kecepatan):
+        nh=(3 * kecepatan) // (16 * ax)
+    elif (40 > kecepatan):
+        nh=0;
+    return nh
+
+def indey(kecepatan, ay):
+    if (kecepatan >=80):
+        ny=20//ay
+    elif (kecepatan >=40 and 80 > kecepatan):
+        ny=(4 * kecepatan)//(16* ay)
+    elif (40 > kecepatan):
+        ny=0
+    return ny
+
+def indek (nx, ny):
+    if(nx>ny):
+        n=ny
+    elif(nx<ny):
+        n=nx
+    elif(nx==ny):
+        n=nx
+    return n
+
+if __name__ == '__main__':
+  gpsp = GpsPoller() # create the thread
+  try:
+    gpsp.start() # start it up
+    adxl345 = ADXL345()
+    f = open('data.txt','a',os.O_NONBLOCK)
+    while True:
+      #It may take a second or two to get good data
+      #print gpsd.fix.latitude,', ',gpsd.fix.longitude,'  Time: ',gpsd.utc
+ 
+      #os.system('clear')
+      axes = adxl345.getAxes(True)
+      print "ADXL345 on address 0x%x:" % (adxl345.address)
+      #print "   x = %.3fG" % ( axes['x'] )
+      f.write("   x = %.3fG" % ( axes['x'] ))
+      f.flush()
+      print "   y = %.3fG" % ( axes['y'] )
+      #f.write ("   y = %.3fG" % ( axes['y'] ))
+      print "   z = %.3fG" % ( axes['z'] )
+      #f.write ("   z = %.3fG" % ( axes['z'] ))
+      print
+      print ' GPS reading'
+      print '----------------------------------------'
+      print 'latitude    ' , gpsd.fix.latitude
+      print 'longitude   ' , gpsd.fix.longitude
+      print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
+      print 'altitude (m)' , gpsd.fix.altitude
+      print 'speed (m/s) ' , gpsd.fix.speed
+      
+ 
+      time.sleep(2) #set to whatever
+ 
+  except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+    print "\nKilling Thread..."
+    gpsp.running = False
+    gpsp.join() # wait for the thread to finish what it's doing
+  print "Done.\nExiting."
+
