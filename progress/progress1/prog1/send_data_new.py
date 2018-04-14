5798fbe331790379195e4bb1d91068506781dfe8
byan21 <vabyan702@gmail.com>
2018-04-14 20:37:54 +0700

    capture

--- progress/progress1/prog1/send_data_new.py
+++ progress/progress1/prog1/send_data_new.py
@@ -2,26 +2,25 @@ import os
 import time
 import MySQLdb
 
-f = open('data.txt', 'r', os.O_NONBLOCK)
+f = open('log.txt', 'r', os.O_NONBLOCK)
 n=0
 while 1:
     for lines in f:
         #lines=f.readline()
         print ("%s"%(lines))
-        a,b,c,d,e,f,g,h,i=lines.split(",")
-        print ("x=%s y=%s va=%s indeks=%s lat=%s lon=%s"%(a,b,c,d,f,g))
+        a,b,c,d,e,f,g=lines.split(",")
+        print ("no =%s x=%s y=%s z=%s va=%s lat=%s lon=%s"%(a,b,c,d,e,f,g))
         db = MySQLdb.connect(host="sql143.main-hosting.eu",   # your host, usually localhost
                          user="u745172280_byan",         # your username
                          passwd="21byan21",  # your password
                          db="u745172280_ta")        # name of the data base
         cur = db.cursor()
         try:
-            cur.execute("INSERT INTO data2_tb (x, y, va,indek, lat, lon) VALUES (%s,%s,%s,%s,%s,%s)",(a, b, c, d, f,g))
+            cur.execute("INSERT INTO data_tes (x, y, z, va, lat, lon) VALUES (%s,%s,%s,%s,%s,%s)",(b, c, d, e,f,g))
             db.commit()
             print ("sukses")
         except:
+            print ("Gagal")
             db.rollback()
         db.close()
         time.sleep(2)
-    
-
